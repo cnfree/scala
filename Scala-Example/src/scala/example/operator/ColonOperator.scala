a39238e163ff370877db16754970aac72ee5348b
cnfree <cnfree2000@hotmail.com>
2016-11-08 23:53:14 +0800

    Add the colon operator example.

---
+++ Scala-Example/src/scala/example/operator/ColonOperator.scala
@@ -0,0 +1,38 @@
+package scala.example.operator
+import scala.language.implicitConversions
+import scala.language.reflectiveCalls
+
+/**
+ * Scala允许用符号作为方法名，用起来像操作符，冒号后缀表示该方法的this参数在右侧。
+ */
+object Json {
+  def |:(obj: Map[_, _]) = {
+    val conc = obj.map { case (t1, t2) => s"  $t1: $t2" }.mkString(",\n")
+    s"{\n$conc\n}"
+  }
+}
+
+object Println {
+  def |:(obj: Any) = println(obj)
+}
+
+object ColonOperator extends App {
+
+  //定义隐式转换方法
+  implicit def pipify[T](t: T) = new {
+    /**
+     * 定义管道方法
+     */
+    def |[R](f: T => R): R = f(t)
+  }
+
+  def json(obj: Map[_, _]) = obj |: Json
+
+  //原始的自定义管道操作符
+  (Map("a" -> 1, "b" -> 2) |: Json) |: Println
+
+  //使用隐式转换的自定义管道操作符
+  Map("a" -> 1, "b" -> 2) | json | println
+
+  "Hello Pipe" | println
+}
